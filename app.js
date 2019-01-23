a15cdfcc4ad52e3239a4f1096e061c315571aebd
labican <flgorgonio@gmail.com>
2019-01-23 02:21:06 -0300

    metodos post, put e delete

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 let express = require('express');
 let app = express();
-const EventoDB = require('./EventoDB')
+const EventoDB = require('./EventoDB');
 let bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    res.send("API Eventos de Bike");
+    res.send("API Calendário de Eventos Ciclísticos");
 });
 
 app.get('/eventos', function (req, res) {
@@ -16,10 +16,39 @@ app.get('/eventos', function (req, res) {
     });
 });
 
+app.get('/eventos/:id(\\d+)', function (req, res) {
+    let id = req.params.id;
+    EventoDB.getEventosById(id, function (evento) {
+        res.json(evento);
+    });
+});
+
+app.delete('/eventos/:id(\\d+)', function (req, res) {
+    let id = req.params.id;
+    console.log("Apagar evento " + id);
+    EventoDB.deleteById(id, function (affectedRows) {
+        res.json({msg: 'Evento apagado com sucesso.'});
+    });
+});
+
 app.get('/eventos/:tipo', function (req, res) {
     let tipo = req.params.tipo;
     EventoDB.getEventosByTipo(tipo, function (eventos) {
         res.json(eventos);
+    });
+});
+
+app.post('/eventos', function (req, res) {
+    let evento = req.body;
+    EventoDB.save(evento, function (evento) {
+        res.json(evento);
+    });
+});
+
+app.put('/eventos', function (req, res) {
+    let evento = req.body;
+    EventoDB.update(evento, function (evento) {
+        res.json(evento);
     });
 });
 
